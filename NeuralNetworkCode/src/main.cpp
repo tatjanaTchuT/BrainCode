23cd451be319cc5a55962b1df93d788511a27717
Antoni Bertolin <tonibm1220@gmail.com>
2023-04-29 15:53:07 +0200

    Update main.cpp

--- NeuralNetworkCode/src/main.cpp
+++ NeuralNetworkCode/src/main.cpp
@@ -172,7 +172,7 @@ int main(int argc, char* argv[])
             neuralNetwork.Simulate();
             neuralNetwork.makeInputCopy(inputFile);
             DatafileParser parser(neuralNetwork.GetRecorder());
-            neuralNetwork.~NeuralNetwork(); //This line is doing shennanigans I think
+            //neuralNetwork.~NeuralNetwork(); //This line is doing shennanigans I think
             parser.parse();
         }
     }
