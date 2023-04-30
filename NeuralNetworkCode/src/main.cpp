9ab4b44fab2c105ad8e56a9a8b8557e4979e9297
Antoni Bertolin <tonibm1220@gmail.com>
2023-04-30 05:05:45 +0200

    Update main.cpp

--- NeuralNetworkCode/src/main.cpp
+++ NeuralNetworkCode/src/main.cpp
@@ -161,9 +161,9 @@ int main(int argc, char* argv[])
 
                 if((parEntry.name.compare("Title") == 0)){
                     if(iterate1_entries[0].name.compare("placeholder") != 0)
-                        parEntry.values[0].append("_i1_"+std::to_string(i1+1));//Removal responds to inability to write to disk when using iterate with long-named parameters
+                        parEntry.values[0].append("_i1_"+std::to_string(i1+1)+"_"+iterate1_entries[0].name+"_"+iterate1_entries[0].values[i1]);
                     if(iterate2_entries[0].name.compare("placeholder") != 0)
-                        parEntry.values[0].append("_i2_"+std::to_string(i2+1));
+                        parEntry.values[0].append("_i2_"+std::to_string(i2+1)+"_"+iterate2_entries[0].name+"_"+iterate2_entries[0].values[i2]);
                 }
             }
             std::cout << "******************************************" << std::endl;
